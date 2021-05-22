625a5469f90d30b0834ed7ea6e68d6b297aa2248
Shihabur Rahman Samrat <shihab150198@gmail.com>
2021-05-22 15:20:55 +0600

    final

---
+++ includes/routes/Routes.php
@@ -0,0 +1,19 @@
+<?php
+
+Route::set('medicines', function() {
+    Medicines::CreateView('Medicines');
+    Medicines::onPageLoad();
+});
+
+Route::set('companies', function() {
+    Companies::CreateView('Companies');
+    Companies::onPageLoad();
+});
+
+Route::set('checkout', function() {
+    Checkout::CreateView('checkout');
+    Checkout::onPageLoad();
+});
+
+
+?>
